9087b0462e136c5486baf6c54dcfc20ce8794b44
Engedi,Emese E <e.engedi@student.fontys.nl>
2017-11-27 01:25:59 +0100

    separated the stylesheet from the master view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,23 +1,6 @@
 @extends('master')
 
-@section('style')
-
-
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        
-    </head>
-    <body>
+@section('content')
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -44,5 +27,4 @@
                 </div>
             </div>
         </div>
-    </body>
-</html>
+@stop
